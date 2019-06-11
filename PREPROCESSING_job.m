6a82a38969e022a07f4d0964a57b8fe7776a2ed8
Victoria Kuryla <victoria.kuryla@gmail.com>
2019-06-11 20:59:06 +0200

    Add files via upload

---
+++ PREPROCESSING_job.m
@@ -0,0 +1,41 @@
+%-----------------------------------------------------------------------
+% Job saved on 11-Jun-2019 18:06:42 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7487)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.realignunwarp.data.scans = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.realignunwarp.data.pmscan = '';
+matlabbatch{1}.spm.spatial.realignunwarp.eoptions.quality = 0.9;
+matlabbatch{1}.spm.spatial.realignunwarp.eoptions.sep = 4;
+matlabbatch{1}.spm.spatial.realignunwarp.eoptions.fwhm = 4;
+matlabbatch{1}.spm.spatial.realignunwarp.eoptions.rtm = 0;
+matlabbatch{1}.spm.spatial.realignunwarp.eoptions.einterp = 2;
+matlabbatch{1}.spm.spatial.realignunwarp.eoptions.ewrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realignunwarp.eoptions.weight = '';
+matlabbatch{1}.spm.spatial.realignunwarp.uweoptions.basfcn = [12 12];
+matlabbatch{1}.spm.spatial.realignunwarp.uweoptions.regorder = 1;
+matlabbatch{1}.spm.spatial.realignunwarp.uweoptions.lambda = 100000;
+matlabbatch{1}.spm.spatial.realignunwarp.uweoptions.jm = 0;
+matlabbatch{1}.spm.spatial.realignunwarp.uweoptions.fot = [4 5];
+matlabbatch{1}.spm.spatial.realignunwarp.uweoptions.sot = [];
+matlabbatch{1}.spm.spatial.realignunwarp.uweoptions.uwfwhm = 4;
+matlabbatch{1}.spm.spatial.realignunwarp.uweoptions.rem = 1;
+matlabbatch{1}.spm.spatial.realignunwarp.uweoptions.noi = 5;
+matlabbatch{1}.spm.spatial.realignunwarp.uweoptions.expround = 'Average';
+matlabbatch{1}.spm.spatial.realignunwarp.uwroptions.uwwhich = [2 1];
+matlabbatch{1}.spm.spatial.realignunwarp.uwroptions.rinterp = 4;
+matlabbatch{1}.spm.spatial.realignunwarp.uwroptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realignunwarp.uwroptions.mask = 1;
+matlabbatch{1}.spm.spatial.realignunwarp.uwroptions.prefix = 'u';
+matlabbatch{2}.spm.spatial.normalise.write.subj.def = '<UNDEFINED>';
+matlabbatch{2}.spm.spatial.normalise.write.subj.resample(1) = cfg_dep('Realign & Unwarp: Unwarped Images (Sess 1)', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','sess', '()',{1}, '.','uwrfiles'));
+matlabbatch{2}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
+                                                          78 76 85];
+matlabbatch{2}.spm.spatial.normalise.write.woptions.vox = [2 2 2];
+matlabbatch{2}.spm.spatial.normalise.write.woptions.interp = 4;
+matlabbatch{2}.spm.spatial.normalise.write.woptions.prefix = 'w';
+matlabbatch{3}.spm.spatial.smooth.data(1) = cfg_dep('Normalise: Write: Normalised Images (Subj 1)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch{3}.spm.spatial.smooth.fwhm = [4 4 4];
+matlabbatch{3}.spm.spatial.smooth.dtype = 0;
+matlabbatch{3}.spm.spatial.smooth.im = 0;
+matlabbatch{3}.spm.spatial.smooth.prefix = 's';
